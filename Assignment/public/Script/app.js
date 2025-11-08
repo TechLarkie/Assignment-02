6256cb41d179b7a050c9291237fc150a045e6509
TechLarkie <zeiad.alarkan@ontariotechu.net>
2025-11-07 22:16:14 -0500

    Final look over of the code and made sure to make comments where possible in order to explain code

--- Assignment/public/Script/app.js
+++ Assignment/public/Script/app.js
@@ -6,6 +6,6 @@
     }
     window.addEventListener("load",start);
     // this adds a listener to the website which is tasked with 'listening' to when the webpage completes its loading 
-    // - once a page has finished loading it will re-run the above segment of code so that the message 'App Started' will reappear in the console log
+    //  once a page has finished loading it will re-run the above segment of code so that the message 'App Started' will reappear in the console log
      
 })();
--- Assignment/routes/index.js
+++ Assignment/routes/index.js
@@ -4,16 +4,18 @@ var router = express.Router();
 /* GET home (/) page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home Page' });
-});
-/*Default landing page upon landing onto the site for the first time*/
+}); /*Default landing page upon landing onto the site for the first time*/
 
 
 /* GET home page */
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home Page' });
-});
-/*Dedicated section for the home landing page which is seperate from the above home page*/
+}); /*Dedicated section for the home landing page which is seperate from the above home page*/
 
+
+
+
+/*The remaining code segments below this comment are all dedicated to creating specific landing pages for each page of the overall website*/
 
 /* GET About Me Page */
 router.get('/aboutme', function(req, res, next) {
